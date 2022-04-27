57d3a1122d3cda5aba39114cf4ad55798487026c
unknown <lidanyang1111@gmail.com>
2022-04-27 14:11:56 -0700

    change

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Breweries from './components/Breweries';
 import Brewery from './components/Brewery';
+import axios from "axios";
 import './App.css';
 
 
+const initialFormValue = {
+  city:"",
+  type:""
+}
+
 function App() {
 
-  const [list, setList] = useState([]);
-  const [formValues, setFormValues] = useState("");
-  const [formErrors, setFormErrors] = useState("");
+  const [BreweriesList, setBreweriesList] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValue);
 
-  const onChange = ()
+
+  const onChange = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues, [inputName]:inputValue
+    })
+  }
 
   const onSubmit = () => {
-    const 
+    const newBrewery = {
+      city:formValues.city,
+      type:formValues.type
+    }
+    axios.post("", newBrewery)
+    .then(res => {
+      setBreweriesList();
+      setFormValues(initialFormValue);
+    })
+    .catch(err=> {
+      console.log(err);
+    })
   }
+
+  useEffect(() => {
+    axios.get(`https://api.openbrewerydb.org/breweries?by_city=${formValues.city}`)
+  },[])
+
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Breweries List</h1>
-        
-
       </header>
+      <Breweries values={formValues} update={onChange} submit={onsubmit} />
+      {breweries.map(brewery => {
+        return (<Brewery key={brewery.id} details={brewery} />)
+      })}
+
     </div>
   );
 }
